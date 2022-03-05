00199633dd659970c17ccdc6a9e03d0e7d91b2a3
39342 <dr_lvr_mail@yahoo.it>
2022-03-05 15:17:23 +0100

    ***

--- backend/insert.php
+++ backend/insert.php
@@ -1,36 +1,49 @@
 <?php
 session_start();
-//$_SESSION['month']=$_POST['month'];
-//$_SESSION['years']=$_POST['years'];
 $servername="localhost";
 $username="root";
 $password="";
 $db="prodotti";
-$conn = mysqli_connect($servername, $username, $password, $db);
+$conn = mysqli_connect($servername, $username, $password);
 if (!$conn){
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-$descrizione=$_POST['descrizione'];
-$reparto=$_POST['reparto'];
-$prezzo=$_POST['prezzo'];
-$quantita=$_POST['quantita'];
-
-//$_SESSION['working_table']=$newtablename;
-$newtablename="frutteria";
-$_SESSION['working_table']=$newtablename;
-$sql = "CALL CREATE_NEW(\"$newtablename\");";
-if ($conn->query($sql) === TRUE) {
-  echo "<script>alert(New table created successfully);</script>";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+else{
+  $sql = "CREATE DATABASE IF NOT EXISTS ".$db;
+  if ($conn->query($sql) === TRUE) {
+    echo "New db created successfully<br>";
+    //here u can associate db and procedures
+  }
+  else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 }
-$sql = "INSERT INTO \"$newtablename\" (descrizione, reparto, prezzo, quantita) VALUES ('$descrizione', '$reparto', '$prezzo', '$quantita')";
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+$conn = mysqli_connect($servername, $username, $password, $db);
+if (!$conn){
+  die("Connection failed: " . mysqli_connect_error()). "<br>";
 }
-$conn->close();
-//header("location: ../index.html");
+else {
+  echo "Connected successfully<br>";
+  $descrizione=$_POST['descrizione'];
+  $reparto=$_POST['reparto'];
+  $prezzo=$_POST['prezzo'];
+  $quantita=$_POST['quantita'];
+  //$_SESSION['working_table']=$newtablename;
+  $newtablename="frutteria";
+  $_SESSION['working_table']=$newtablename;
+  $sql = "CALL CREATE_NEW(\"$newtablename\");";
+  if ($conn->query($sql) === TRUE) {
+    echo "New table created successfully<br>";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $sql = "INSERT INTO ".$newtablename." (descrizione, reparto, prezzo, quantita) VALUES ('$descrizione', '$reparto', '$prezzo', '$quantita')";
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+  header("location: ../index.php");
+}
 ?>
--- backend/show.php
+++ backend/show.php
@@ -9,48 +9,48 @@
 <body>
   <script>
   function clickMe(){
-  var result ="<?php php_func(); ?>"
-  document.write(result);
+    var result ="<?php php_func(); ?>"
+    document.write(result);
   }
   </script>
-<?php
-function php_func(){
-echo "Stay Safe";
-}
-$servername="localhost";
-$username="root";
-$password="";
-$db="prodotti";
-$conn = mysqli_connect($servername, $username, $password, $db);
-if (!$conn){
-  die("Connection failed: " . mysqli_connect_error());
-}
-//$table=$_SESSION['working_table'];
-//echo var_dump($table);
-$Sql = "SELECT * FROM frutteria WHERE 1";
-          $result = mysqli_query($conn, $Sql);
-          if (mysqli_num_rows($result) > 0) {
-            echo "<div class='table-responsive'><table id='myTable' class='table table-striped table-bordered'>
-            <thead><th>id</th>
-            <th>descrizione</th>
-            <th>reparto</th>
-            <th>prezzo</th>
-            <th>quantità</th>
-            </tr></thead><tbody>";
-            while($row = mysqli_fetch_assoc($result)) {
-              echo "<tr id=\"".$row['id']."\" name=\"".$row['id']."\"><td>" . $row['id']."</td>
-              <td>" . $row['descrizione']."</td>
-              <td>" . $row['reparto']."</td>
-              <td>" . $row['prezzo']."</td>
-              <td>" . $row['quantita']."</td>
-              <td onclick=\"clickMe()\">modify</td>
-              <td>delete</td>
-              </tr>";
-            }
-            echo "</tbody></table></div>";
-          } else {
-            echo "you have no records";
-          }
-?>
+  <?php
+  function php_func(){
+    echo "";
+  }
+  $servername="localhost";
+  $username="root";
+  $password="";
+  $db="prodotti";
+  $conn = mysqli_connect($servername, $username, $password, $db);
+  if (!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  $table=$_SESSION['working_table'];
+  $Sql = "SELECT * FROM ".$table." WHERE 1";
+  $result = mysqli_query($conn, $Sql);
+  if (mysqli_num_rows($result) > 0) {
+    echo "<div class='table-responsive'><table id='myTable' class='table table-striped table-bordered'>
+    <thead><th>id</th>
+    <th>descrizione</th>
+    <th>reparto</th>
+    <th>prezzo</th>
+    <th>quantità</th>
+    </tr></thead><tbody>";
+    while($row = mysqli_fetch_assoc($result)) {
+      echo "<tr id=\"".$row['ID']."\" name=\"".$row['ID']."\">
+      <td>" . $row['ID']."</td>
+      <td>" . $row['descrizione']."</td>
+      <td>" . $row['reparto']."</td>
+      <td>" . $row['prezzo']."</td>
+      <td>" . $row['quantita']."</td>
+      <td onclick=\"clickMe()\">modify</td>
+      <td>delete</td>
+      </tr>";
+    }
+    echo "</tbody></table></div>";
+  } else {
+    echo "you have no records";
+  }
+  ?>
 </body>
 </html>
